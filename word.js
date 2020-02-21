823728302770f0dfb1c7c8a15d6f73cedd4f2eae
Rachel Sipes <rasipes@gmail.com>
2020-02-20 19:41:04 -0500

    word.js code written following tutorial

--- word.js
+++ word.js
@@ -1,12 +1,34 @@
 var Letter = require("./letter");
 // contains a constructor
 
+var Word = function(answer) {
+    this.objArray = [];
+
+    for(var i = 0; i < answer.length; i++){
+        var letter = new Letter(answer[i]);
+        this.objArray.push(letter);
+    }
+    this.log = function() {
+        var answerLog = "";
+        for(var i = 0; i < this.objArray.length; i++) {
+            answerLog += this.objArray[i] + " ";
+        }
+        console.log(answerLog + "\n===================\n");
+    }
+    this.userInput = function(input) {
+        for (var i = 0; i < this.objArray.length; i++) {
+            this.objArray[i].guess(input);
+        }
+    }
+}
 
 
 module.exports = Word;
 
 
 var words = ["Jurassic Park", "Star Wars", "Frozen"]
+
+
 // function randomWordGen() {
 
 // }
